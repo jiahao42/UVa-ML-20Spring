bd51d4ab202076e3f45bfcb4e573a060384c1b83
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-03 01:54:46 -0400

    new graph edit dist algo

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@
 # @Description: 
 
 import networkx as nx
+# import graphsim as gs
+from ged4py.algorithm import graph_edit_dist
 from math import exp
 import numpy as np
 import pickle
@@ -12,22 +14,25 @@ import pickle
 # see https://networkx.github.io/documentation/stable/reference/algorithms/similarity.html
 def edit_distance(G1, G2):
   # res = nx.graph_edit_distance(G1, G2)
-  res = nx.optimize_graph_edit_distance(G1, G2)
-  for v in res:
+  # res = nx.optimize_graph_edit_distance(G1, G2)
+  res = graph_edit_dist.compare(G1, G2)
+  print(res)
+  return res
+  # for v in res:
     # print(v)
-    return v
+    # return v
   # return nx.optimal_edit_paths(G1, G2)
   # return nx.simrank_similarity_numpy(G1, G2)
 
-def sigmoid(x):
+def sigmoid(x, base = 1.0):
     if x >= 0:
         z = exp(-x)
         ret = 1 / (1 + z)
-        return 1.0 if np.isnan(ret) else ret
+        return base if np.isnan(ret) else ret * base
     else:
         z = exp(x)
         ret = z / (1 + z)
-        return -1.0 if np.isnan(ret) else ret
+        return -base if np.isnan(ret) else ret * base
 
 def load_data(data_files, path = 'dataset/'):
   data = []
